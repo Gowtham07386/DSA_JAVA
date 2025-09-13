1cfd5c34e531f6c66c5f0444dda2a8947185d498
Gowtham07386 <kopperagowtham@gmail.com>
2025-09-13 15:52:49 +0530

    Jagged Array

---
+++ Arrays_java/Jagged_array.java
@@ -0,0 +1,36 @@
+public class Jagged_array {
+    public static void main(String[] args) {
+        int arr[][] = new int[3][];
+
+        arr[0]=new int[5];
+        arr[1]=new int[4];
+        arr[2]=new int[3];
+
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                arr[i][j] =(int)(Math.random()*100);
+
+            }
+            
+        }
+        // for(int i=0;i<arr.length;i++){
+        //     for(int j=0;j<arr[i].length;j++){
+        //         System.out.print(arr[i][j]+" ");
+
+        //     }
+        //     System.out.println();
+            
+        // }
+        for(int n[] : arr){
+            for(int m : n){
+                System.out.print(m +" ");
+
+            }
+            System.out.println();
+        }
+
+
+
+    }
+    
+}
