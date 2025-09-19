1e15d6294bc92249064a073150e4c5b72f8b48d5
Gowtham07386 <kopperagowtham@gmail.com>
2025-09-19 22:13:54 +0530

    Creating string

---
+++ Strings_java/string_cre.java
@@ -0,0 +1,12 @@
+public class string_cre {
+    public static void main(String[] args) {
+        String name = "Gowtham Koppera";
+
+        String name1 = new String("HareKrishna");
+         System.out.println(name);
+         System.out.println(name1);
+         System.out.println(name.charAt(5)); //To find character from the index place
+         System.out.println(name1.concat(" "+name));
+    }
+    
+}
